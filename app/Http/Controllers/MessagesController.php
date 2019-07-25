92ced4c9585c37afd90c14b646adf6d0b5e6797c
excellencetechnologies <excellencetechnologies>
2019-07-25 18:15:02 +0530

    generating text messages by substituting variables and send slack message

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -8,54 +8,6 @@ use Illuminate\Http\Request;
 
 class MessagesController extends Controller
 {
-    // add or update message
-    public function addMessage()
-    {
-        try {
-            $params = request()->all();
-            $message = new Messages();
-            $data = $message->addMessage($params);
-            $response = [ 'error' => 0, 'data' => $data ];
-
-        } catch( Exception $ex ) {
-            $response = [ 'error' => 1, 'message' => $ex->getMessage() ];
-        }
-
-        return response()->json($response);
-    }
-
-    // all messages
-    public function allMessages()
-    {
-        try {
-            $params = request()->all();
-            $message = new Messages();
-            $data = $message->allMessages($params);
-            $response = [ 'error' => 0, 'data' => $data ];
-
-        } catch( Exception $ex ) {
-            $response = [ 'error' => 1, 'message' => $ex->getMessage() ];
-        }
-
-        return response()->json($response);
-    }
-
-    // delete message
-    public function deleteMessage()
-    {
-        try {
-            $id = request()->id;
-            $message = new Messages();
-            $data = $message->deleteMessage($id);
-            $response = [ 'error' => 0, 'data' => $data ];
-
-        } catch( Exception $ex ) {
-            $response = [ 'error' => 1, 'message' => $ex->getMessage() ];
-        }
-
-        return response()->json($response);
-    }
-
     // send message
     public function sendMessage()
     {
--- app/Messages.php
+++ app/Messages.php
@@ -27,94 +27,57 @@ class Messages extends Model
      */
     protected $hidden = ['created_at', 'updated_at'];
 
-
-    // add or update message
-    public function addMessage($params)
-    {
-        $validator = Validator::make($params, [
-            'app_name' => 'required',
-            'identifier' => 'required',
-            'text' => 'required'
-        ]);
-
-        if( $validator->fails() ){
-            throw new Exception( $validator->errors()->first() );
-        }
-
-        $system = SystemApp::where('app_name', $params['app_name'])->get();
-        if( $system->count() < 1 ){
-            throw new Exception("System not found");
-        }
-
-        if( array_key_exists('attachments', $params) ){
-            $params['attachments'] = json_encode($params['attachments']);
-        }
-        
-        $message = Messages::updateOrCreate([
-                'system_id' => $system->first()->id,
-                'identifier' => $params['identifier'],
-            ], 
-            $params
-        )->toArray();
-
-        return $message;
-    }
-
-    // all messages
-    public function allMessages($params)
-    {
-        $validator = Validator::make($params, [
-            'app_name' => 'required'
-        ]);
-
-        if( $validator->fails() ){
-            throw new Exception( $validator->errors()->first() );
-        }
-
-        $system = SystemApp::where('app_name', $params['app_name'])->get();
-        if( $system->count() < 1 ){
-            throw new Exception("System not found");
-        }
-
-        $message = Messages::where('system_id', $system->first()->id)->get()->toArray();
-        return $message;
-    }
-
-    // delete message
-    public function deleteMessage($id)
-    {
-        $message = Messages::find($id);
-        if( !$message ){
-            throw new Exception("Message not found");
-
-        } else {
-            $message->delete();
-        }
-
-        return $message;
-    }
-
     // send message
     public function sendMessage($params)
     {
         $validator = Validator::make($params, [
-            'text' => 'required',
-            'channel' => 'required'
+            'app_name' => 'required',
+            'identifier' => 'required',
+            'channel' => 'required',
+            'variables' => 'required|array'
         ]);
 
         if( $validator->fails() ){
             throw new Exception( $validator->errors()->first() );
         }
 
-        $url = env('SLACK_POST_MESSAGE_URL');
-
-        $params['token'] = env('SLACK_TOKEN');
-        if( array_key_exists('attachments', $params) ){
-            $params['attachments'] = json_encode($params['attachments']);
+        $app = SystemApp::where('app_name', $params['app_name'])->get();
+        if( $app->count() < 1 ){
+            throw new Exception($params['app_name'] . " not found");
         }
+
+        $message = Messages::where('system_id', $app->first()->id)->where('identifier', $params['identifier'])->get();
+        if( $message->count() < 1 ){
+            throw new Exception("No message found by identifier " . $params['identifier']);
+        }
+
+        $raw_text = explode(" ", $message->first()->text);
+
+        // check for all needed variables and generating the text.
+        foreach( $raw_text as $index => $variable ){
+            if( strpos($variable, '#') !== false ){
+                $key = str_replace('#', '', $variable);
+                if( !array_key_exists($key, $params['variables']) ){
+                    throw new Exception( $key . " is a required variable." );                                        
+                } else {
+                    if( !$params['variables'][$key] ){
+                        throw new Exception( $key . " is a required variable." );
+                    }
+                    $raw_text[$index] = $params['variables'][$key];
+                }  
+            }
+        }
+
+        $text = implode(" ", $raw_text);
+
+        $payload = [];
+        $payload['token'] = env('SLACK_TOKEN');
+        $payload['text'] = $text;
+        $payload['channel'] = $params['channel'];
+
         $client = new Client();
-        $response = $client->request('POST', $url, [
-            'form_params' => $params
+        $response = $client->request('POST', env('SLACK_POST_MESSAGE_URL'), [
+            'form_params' => $payload
         ]);
         
         return json_decode($response->getBody()->getContents(), true);
